f41f34642c91c33718ad8324f1dd95c00c21cd02
mtahir <mo.tahir@contino.io>
2017-07-19 15:59:53 +0100

    Removed some invalid variables

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "azurerm" {
-  subscription_id = "${var.azure_subscription}"
-  client_id       = "${var.azure_client_id}"
-  client_secret   = "${var.azure_client_secret}"
-  tenant_id       = "${var.azure_tenant_id}"
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "${var.name}-${var.env}"
   location = "${var.location}"
