45cc2c40f39f12e68a5070a4286fb023a68d219f
Mo Tahir <motahir@users.noreply.github.com>
2017-09-27 21:12:04 +0100

    Added waf to the core infrastructure (#12)

--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ module "vnet" {
   address_prefixes = "${var.address_prefixes}"
   env              = "${var.env}"
 }
+
+module "waf" {
+  source         = "git::https://66ef3c054a0798d24a36f274c19041e92832687c@github.com/contino/moj-module-waf?ref=master"
+  product        = "${var.name}"
+  location       = "${var.location}"
+  env            = "${var.env}"
+  vnetname       = "${module.vnet.vnet_id}"
+  subnetname     = "${module.vnet.subnet_names[0]}"
+  backendaddress = "0.0.0.0"
+}
