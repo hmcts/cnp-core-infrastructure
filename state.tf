af0e8f750e250d73a08edd6a3374f3432ea8e1f1
Calin Marina <calin.marina@gmail.com>
2017-10-04 11:29:52 +0100

    fix state storage key name

--- state.tf
+++ state.tf
@@ -9,6 +9,6 @@ data "terraform_remote_state" "vnetA_state" {
     resource_group_name  = "${var.vnetA_state_rg_name}"
     storage_account_name = "${var.vnetA_state_storageAccount_name}"
     container_name       = "${var.vnetA_state_container_name}"
-    key                  = "${var.vnetA_state_key}/terraform.tfstate"
+    key                  = "${var.vnetA_state_key}"
   }
 }
