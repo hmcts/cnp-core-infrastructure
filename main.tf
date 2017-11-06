776c2e43be9764033c268a44161493d0335e5b83
James Whinn <jameswhinn89@gmail.com>
2017-11-06 07:40:50 +0000

    fix build

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vnet" {
-  source           = "git::https://66ef3c054a0798d24a36f274c19041e92832687c@github.com/contino/moj-module-vnet?ref=master"
+  source           = "git::git@github.com:contino/moj-module-vnet?ref=master"
   name             = "${var.name}"
   location         = "${var.location}"
   address_space    = "${var.address_space}"
@@ -8,7 +8,7 @@ module "vnet" {
 }
 
 module "waf" {
-  source            = "git::https://66ef3c054a0798d24a36f274c19041e92832687c@github.com/contino/moj-module-waf?ref=master"
+  source            = "git::git@github.com:contino/moj-module-waf?ref=master"
   product           = "${var.name}"
   location          = "${var.location}"
   env               = "${var.env}"
