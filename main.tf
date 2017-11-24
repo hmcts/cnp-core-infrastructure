7a16db429469d913675988a85472cb93ef97d6b0
James Whinn <jameswhinn89@gmail.com>
2017-11-24 13:12:29 +0000

    correct naming of vm img container

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ module "consul" {
   allowed_inbound_cidr_blocks = ["10.0.0.0/16"]
   key_data                    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDM/3NgLgH7/a6GZQ10O3PbzVMqM7hXPrRFXONaBRpIdSoBmRJGY531f4QPMKc4uS8PUqA8ClZ5MIMzqgD4zAmsB8eEnvEQ2dE7lW4rnaVphgoNr/MqxU3AQJsoJypOjDNp9xrjQqlco3OC2Ro1f+yOzj6FwonoGfD0I5DGtyKbSb1Pv8a8DbXILxju1ayBHCqwmhYCxrEH3yJlnB+KF/J5Q5u5oczsZBqzp0qC58TUpGkVqxFAk3zPwZxqny6xKNgGO9/gMKVjPMmTTiTa3MQHReG5JUSrHiMZe23/+QAClsflEAxhJeGf2h2jIBo3aQpcuc7ULzq2OhGG7dJ0VWwL"
   resource_group_name         = "${module.vnet.resourcegroup_name}"
-  image_uri                   = "/subscriptions/8999dec3-0104-4a27-94ee-6588559729d1/resourceGroups/mgmtvmimgstoreprd/providers/Microsoft.Compute/images/moj-centos-consul-07112017"
+  image_uri                   = "/subscriptions/8999dec3-0104-4a27-94ee-6588559729d1/resourceGroups/mgmt-vmimg-store-prd/providers/Microsoft.Compute/images/moj-centos-consul-07112017"
   location                    = "${var.location}"
   cluster_name                = "consul"
 }
