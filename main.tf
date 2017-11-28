7794375fea36e1b713ddf1d5c15f0d768792d8ce
James Whinn <jameswhinn89@gmail.com>
2017-11-28 10:49:51 +0000

    switch vnet ton private-ase branch

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vnet" {
-  source           = "git::git@github.com:contino/moj-module-vnet?ref=master"
+  source           = "git::git@github.com:contino/moj-module-vnet?ref=private-ase"
   name             = "${var.name}"
   location         = "${var.location}"
   address_space    = "${var.address_space}"
