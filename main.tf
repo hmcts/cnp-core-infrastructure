cc30a5b7e1051e7ddea05727980ce86610d31cc6
sanabriad <sanabria.d@gmail.com>
2017-11-29 15:57:10 +0000

    Rebuilding cluster with new image

--- main.tf
+++ main.tf
@@ -20,20 +20,20 @@ module "waf" {
  // pfxPass           = "${var.pfxPass}"
 }
 
-#module "consul" {
-#  source                      = "git::git@github.com:contino/moj-module-consul?ref=master"
-#  subscription_id             = "${var.subscription_id}"
-#  tenant_id                   = "${var.tenant_id}"
-#  client_id                   = "${var.client_id}"
-#  secret_access_key           = "${var.secret_access_key}"
-#  subnet_id                   = "${module.vnet.subnet_ids[2]}"
-#  storage_account_name        = "mgmtvmimgstore${var.env}"
-#  allowed_inbound_cidr_blocks = ["10.98.0.0/16"]
-#  key_data                    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDM/3NgLgH7/a6GZQ10O3PbzVMqM7hXPrRFXONaBRpIdSoBmRJGY531f4QPMKc4uS8PUqA8ClZ5MIMzqgD4zAmsB8eEnvEQ2dE7lW4rnaVphgoNr/MqxU3AQJsoJypOjDNp9xrjQqlco3OC2Ro1f+yOzj6FwonoGfD0I5DGtyKbSb1Pv8a8DbXILxju1ayBHCqwmhYCxrEH3yJlnB+KF/J5Q5u5oczsZBqzp0qC58TUpGkVqxFAk3zPwZxqny6xKNgGO9/gMKVjPMmTTiTa3MQHReG5JUSrHiMZe23/+QAClsflEAxhJeGf2h2jIBo3aQpcuc7ULzq2OhGG7dJ0VWwL"
-#  resource_group_name         = "${module.vnet.resourcegroup_name}"
-#  image_uri                   = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/mgmt-vmimg-store-nonprod/providers/Microsoft.Compute/images/moj-centos-consul-241117-161902"
-#  location                    = "${var.location}"
-#  cluster_name                = "consul"
-#  lb_private_ip_address       = "${var.lb_private_ip_address}"
-#
-#}
+module "consul" {
+  source                      = "git::git@github.com:contino/moj-module-consul?ref=master"
+  subscription_id             = "${var.subscription_id}"
+  tenant_id                   = "${var.tenant_id}"
+  client_id                   = "${var.client_id}"
+  secret_access_key           = "${var.secret_access_key}"
+  subnet_id                   = "${module.vnet.subnet_ids[2]}"
+  storage_account_name        = "mgmtvmimgstore${var.env}"
+  allowed_inbound_cidr_blocks = ["10.98.0.0/16"]
+  key_data                    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDM/3NgLgH7/a6GZQ10O3PbzVMqM7hXPrRFXONaBRpIdSoBmRJGY531f4QPMKc4uS8PUqA8ClZ5MIMzqgD4zAmsB8eEnvEQ2dE7lW4rnaVphgoNr/MqxU3AQJsoJypOjDNp9xrjQqlco3OC2Ro1f+yOzj6FwonoGfD0I5DGtyKbSb1Pv8a8DbXILxju1ayBHCqwmhYCxrEH3yJlnB+KF/J5Q5u5oczsZBqzp0qC58TUpGkVqxFAk3zPwZxqny6xKNgGO9/gMKVjPMmTTiTa3MQHReG5JUSrHiMZe23/+QAClsflEAxhJeGf2h2jIBo3aQpcuc7ULzq2OhGG7dJ0VWwL"
+  resource_group_name         = "${module.vnet.resourcegroup_name}"
+  image_uri                   = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/mgmt-vmimg-store-nonprod/providers/Microsoft.Compute/images/moj-centos-consul-291117-150338"
+  location                    = "${var.location}"
+  cluster_name                = "consul"
+  lb_private_ip_address       = "${var.lb_private_ip_address}"
+
+}
