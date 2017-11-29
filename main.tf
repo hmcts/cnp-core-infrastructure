a828b0ad4ff42b7fd84f954a6e73621abe1e3033
Calin Marina <calin.marina@gmail.com>
2017-11-29 11:41:40 +0000

    fix hardcoded storage account for consul

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "consul" {
   client_id                   = "${var.client_id}"
   secret_access_key           = "${var.secret_access_key}"
   subnet_id                   = "${module.vnet.subnet_ids[2]}"
-  storage_account_name        = "mgmtvmimgstoreprd"
+  storage_account_name        = "mgmtvmimgstore${var.env}"
   allowed_inbound_cidr_blocks = ["10.98.0.0/16"]
   key_data                    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDM/3NgLgH7/a6GZQ10O3PbzVMqM7hXPrRFXONaBRpIdSoBmRJGY531f4QPMKc4uS8PUqA8ClZ5MIMzqgD4zAmsB8eEnvEQ2dE7lW4rnaVphgoNr/MqxU3AQJsoJypOjDNp9xrjQqlco3OC2Ro1f+yOzj6FwonoGfD0I5DGtyKbSb1Pv8a8DbXILxju1ayBHCqwmhYCxrEH3yJlnB+KF/J5Q5u5oczsZBqzp0qC58TUpGkVqxFAk3zPwZxqny6xKNgGO9/gMKVjPMmTTiTa3MQHReG5JUSrHiMZe23/+QAClsflEAxhJeGf2h2jIBo3aQpcuc7ULzq2OhGG7dJ0VWwL"
   resource_group_name         = "${module.vnet.resourcegroup_name}"
