4e5f0e9ed649077436409628c794585e35e9c400
James Whinn <jameswhinn89@gmail.com>
2018-01-05 09:17:43 +0000

    test

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "vnet" {
   source           = "git::git@github.com:contino/moj-module-vnet?ref=dynamic_subnet"
   name             = "${var.name}"
   location         = "${var.location}"
-  address_space    = ["${var.vnetiprange}"]
+  address_space    = "${var.vnetiprange}"
   source_range     = "${var.vnetiprange}"
  // address_prefixes = ["${cidrsubnet("${var.vnetiprange}", 7, 0)}","${cidrsubnet("${var.vnetiprange}", 7, 1)}","${cidrsubnet("${var.vnetiprange}", 7, 2)}","${cidrsubnet("${var.vnetiprange}", 7, 3)}"]
  // address_prefixes = ["${cidrsubnet("${module.vnet.address_space}", 7, 0)}","${cidrsubnet("${module.vnet.address_space}", 7, 1)}","${cidrsubnet("${module.vnet.address_space}", 7, 2)}","${cidrsubnet("${module.vnet.address_space}", 7, 3)}"]
