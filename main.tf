f6bba0f05729017e99e5ef429eff8967707de34d
Liam F. O'Neill <liamfoneill@users.noreply.github.com>
2018-11-02 14:10:02 +0000

    Update main.tf
    updated to reference a specific consul release

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ module "waf" {
 }
 
 module "consul" {
-  source                      = "git::git@github.com:contino/moj-module-consul?ref=master"
+  source                      = "git::git@github.com:contino/moj-module-consul?ref=0.1.6"
   subscription_id             = "${var.subscription_id}"
   tenant_id                   = "${var.tenant_id}"
   client_id                   = "${var.client_id}"
