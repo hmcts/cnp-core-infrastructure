ade489e87395e778a2fc3f8eb0d297bd252020b3
Leonardo Fernandez <leonfs@gmail.com>
2018-11-12 12:00:55 +0000

    Consul image selection uses data sources. Cleaned up the Jenkinsfile, removed the consul image selection from the pipeline.

--- main.tf
+++ main.tf
@@ -28,6 +28,13 @@ module "waf" {
   resourcegroupname = "${module.vnet.resourcegroup_name}"
 }
 
+
+data "azurerm_image" "consul" {
+  resource_group_name = "cnp-vmimages-${var.subscription}"
+  name_regex = "moj-centos-consul-0.1.6"
+  sort_descending = true
+}
+
 module "consul" {
   source                      = "git::git@github.com:hmcts/cnp-module-consul?ref=0.1.6"
   subscription_id             = "${var.subscription_id}"
@@ -39,7 +46,7 @@ module "consul" {
   allowed_inbound_cidr_blocks = ["${cidrsubnet("${var.root_address_space}", 6, "${var.netnum}")}"]
   key_data                    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDM/3NgLgH7/a6GZQ10O3PbzVMqM7hXPrRFXONaBRpIdSoBmRJGY531f4QPMKc4uS8PUqA8ClZ5MIMzqgD4zAmsB8eEnvEQ2dE7lW4rnaVphgoNr/MqxU3AQJsoJypOjDNp9xrjQqlco3OC2Ro1f+yOzj6FwonoGfD0I5DGtyKbSb1Pv8a8DbXILxju1ayBHCqwmhYCxrEH3yJlnB+KF/J5Q5u5oczsZBqzp0qC58TUpGkVqxFAk3zPwZxqny6xKNgGO9/gMKVjPMmTTiTa3MQHReG5JUSrHiMZe23/+QAClsflEAxhJeGf2h2jIBo3aQpcuc7ULzq2OhGG7dJ0VWwL"
   resource_group_name         = "${module.vnet.resourcegroup_name}"
-  image_uri                   = "${var.vmimage_uri}"
+  image_uri                   = "${data.azurerm_image.consul.id}"
   location                    = "${var.location}"
   cluster_name                = "consul"
   lb_private_ip_address       = "${cidrhost("${cidrsubnet("${cidrsubnet("${var.root_address_space}", 6, "${var.netnum}")}", 4, 2)}", -2)}"
