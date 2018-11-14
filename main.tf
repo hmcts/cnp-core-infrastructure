5d2333cd20706cfeaf5f02afb38140057caac092
daniele vottero <daniele.vottero@contino.io>
2018-11-14 14:04:39 +0000

    use consul image built with vsts (#49)
    * Consul image selection uses data sources. Cleaned up the Jenkinsfile, removed the consul image selection from the pipeline.
    
    * Removed vmimage_uri variable, now it's being picked up using a terraform data source
    
    * point to tag 0.1.7 consul module
    
    * centos consul image version updated

--- main.tf
+++ main.tf
@@ -28,8 +28,15 @@ module "waf" {
   resourcegroupname = "${module.vnet.resourcegroup_name}"
 }
 
+
+data "azurerm_image" "consul" {
+  resource_group_name = "cnp-vmimages-${var.subscription}"
+  name_regex = "moj-centos-consul-0.1.7"
+  sort_descending = true
+}
+
 module "consul" {
-  source                      = "git::git@github.com:contino/cnp-module-consul?ref=0.1.6"
+  source                      = "git::git@github.com:hmcts/cnp-module-consul?ref=0.1.7"
   subscription_id             = "${var.subscription_id}"
   tenant_id                   = "${var.tenant_id}"
   client_id                   = "${var.client_id}"
@@ -39,14 +46,14 @@ module "consul" {
   allowed_inbound_cidr_blocks = ["${cidrsubnet("${var.root_address_space}", 6, "${var.netnum}")}"]
   key_data                    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDM/3NgLgH7/a6GZQ10O3PbzVMqM7hXPrRFXONaBRpIdSoBmRJGY531f4QPMKc4uS8PUqA8ClZ5MIMzqgD4zAmsB8eEnvEQ2dE7lW4rnaVphgoNr/MqxU3AQJsoJypOjDNp9xrjQqlco3OC2Ro1f+yOzj6FwonoGfD0I5DGtyKbSb1Pv8a8DbXILxju1ayBHCqwmhYCxrEH3yJlnB+KF/J5Q5u5oczsZBqzp0qC58TUpGkVqxFAk3zPwZxqny6xKNgGO9/gMKVjPMmTTiTa3MQHReG5JUSrHiMZe23/+QAClsflEAxhJeGf2h2jIBo3aQpcuc7ULzq2OhGG7dJ0VWwL"
   resource_group_name         = "${module.vnet.resourcegroup_name}"
-  image_uri                   = "${var.vmimage_uri}"
+  image_uri                   = "${data.azurerm_image.consul.id}"
   location                    = "${var.location}"
   cluster_name                = "consul"
   lb_private_ip_address       = "${cidrhost("${cidrsubnet("${cidrsubnet("${var.root_address_space}", 6, "${var.netnum}")}", 4, 2)}", -2)}"
 }
 
 module "api-mgmt" {
-  source             = "git@github.com:hmcts/moj-module-api-mgmt?ref=master"
+  source             = "git@github.com:hmcts/cnp-module-api-mgmt?ref=0.1.0"
   location           = "${var.location}"
   env                = "${var.env}"
   subscription       = "${var.subscription}"
