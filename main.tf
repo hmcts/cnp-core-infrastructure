e6cc73fec4fd75e1b9ac12cefc2f798859cb524f
danielev04 <daniele@vottero.eu>
2019-02-28 10:01:32 +0000

    address space from tfvars

--- main.tf
+++ main.tf
@@ -43,14 +43,14 @@ module "consul" {
   secret_access_key           = "${var.secret_access_key}"
   subnet_id                   = "${module.vnet.subnet_ids[2]}"
   storage_account_name        = "mgmtvmimgstore${var.env}"
-  allowed_inbound_cidr_blocks = ["${cidrsubnet("${var.root_address_space}", 6, "${var.netnum}")}"]
+  allowed_inbound_cidr_blocks = ["${var.address_space}"]
   key_data                    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDM/3NgLgH7/a6GZQ10O3PbzVMqM7hXPrRFXONaBRpIdSoBmRJGY531f4QPMKc4uS8PUqA8ClZ5MIMzqgD4zAmsB8eEnvEQ2dE7lW4rnaVphgoNr/MqxU3AQJsoJypOjDNp9xrjQqlco3OC2Ro1f+yOzj6FwonoGfD0I5DGtyKbSb1Pv8a8DbXILxju1ayBHCqwmhYCxrEH3yJlnB+KF/J5Q5u5oczsZBqzp0qC58TUpGkVqxFAk3zPwZxqny6xKNgGO9/gMKVjPMmTTiTa3MQHReG5JUSrHiMZe23/+QAClsflEAxhJeGf2h2jIBo3aQpcuc7ULzq2OhGG7dJ0VWwL"
   resource_group_name         = "${module.vnet.resourcegroup_name}"
   image_uri                   = "${data.azurerm_image.consul.id}"
   location                    = "${var.location}"
   cluster_name                = "consul"
   instance_size               = "Standard_A2_v2"
-  lb_private_ip_address       = "${cidrhost("${cidrsubnet("${cidrsubnet("${var.root_address_space}", 6, "${var.netnum}")}", 4, 2)}", -2)}"
+  lb_private_ip_address       = "${cidrhost("${cidrsubnet("${var.address_space}", 4, 2)}", -2)}"
 }
 
 module "api-mgmt" {
@@ -60,6 +60,6 @@ module "api-mgmt" {
   subscription       = "${var.subscription}"
   vnet_rg_name       = "${module.vnet.resourcegroup_name}"
   vnet_name          = "${module.vnet.vnetname}"
-  source_range       = "${cidrsubnet("${var.root_address_space}", 6, "${var.netnum}")}"
+  source_range       = "${var.address_space}"
   source_range_index = "${length(module.vnet.subnet_ids)}"
 }
