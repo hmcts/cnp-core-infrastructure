0a49191bbf5f8454693257718853ec975f109108
Hannah Smith <hannahs@kainos.com>
2020-08-17 09:12:05 +0100

    change tags to include environment

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ module "vnet" {
   source_range          = "${var.address_space}"
   env                   = "${var.env}"
   lb_private_ip_address = "${cidrhost("${cidrsubnet("${var.address_space}", 4, 2)}", -2)}"
+
+  common_tags           = { 
+    "environment" = "${var.env}" 
+  }
 }
 
 module "api-mgmt" {
@@ -17,4 +21,8 @@ module "api-mgmt" {
   vnet_name          = "${module.vnet.vnetname}"
   source_range       = "${var.address_space}"
   source_range_index = "${length(module.vnet.subnet_ids)}"
+
+  common_tags           = {
+    "environment" = "${var.env}"
+  }
 }
