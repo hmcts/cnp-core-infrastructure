f6e3190a7f8365383eec98f8fcec4ef28d1e4deb
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-10-13 14:26:03 +0100

    make subnets conditional

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "vnet" {
   env                           = var.env
   lb_private_ip_address         = cidrhost(cidrsubnet(var.address_space, 4, 2), -2)
   postgresql_subnet_cidr_blocks = var.postgresql_subnet_cidr_blocks
-  additional_subnets            = local.additional_subnets
+  additional_subnets            = var.env == "aat" || "prod" ? local.additional_subnets : []
 
   common_tags = module.ctags.common_tags
 }
