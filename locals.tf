aa63356b591469d4d361dea7367642ab2fee7376
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-10-13 14:15:50 +0100

    update locals

--- locals.tf
+++ locals.tf
@@ -1,13 +1,16 @@
 locals {
-  additional_subnets = {
-    postgresql = {
-      address_prefixes = [var.postgresql_subnet_prefix]
+  additional_subnets = [
+    {
+      name              = "postgresql"
+      address_prefixes  = [var.postgresql_subnet_prefix]
+      service_endpoints = ["Microsoft.Storage", "Microsoft.Sql"]
       delegations = {
         fs = {
+          name         = "fs"
           service_name = "Microsoft.DBforPostgreSQL/flexibleServers"
-          actions      = ["Microsoft.Network/virtualNetworks/subnets/action"]
+          actions      = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
         }
       }
     }
-  }
+  ]
 }
